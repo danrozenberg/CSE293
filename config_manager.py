644921f21edb663a5a23eb92a7178934b1cb09fc
Dan Rozenberg <danrozenberg@gmail.com>
2016-03-31 16:26:39 -0700

    dr: add a simple config manager

---
+++ config_manager.py
@@ -0,0 +1,19 @@
+#TODO: upgrade to some configuration/environment helper library
+class Config(object):
+
+    def __init__(self):
+        self.config_folder_path = "./config/"
+        self.data_path = None
+        self.__setup_environment()
+
+    def get_data_path(self):
+        return self.data_path
+
+    def __setup_environment(self):
+        f = open(self.config_folder_path + "environment.txt")
+        for line in f:
+            split_line = line.split("=")
+            if split_line[0] == "data_path":
+                self.data_path = split_line[1]
+
+
