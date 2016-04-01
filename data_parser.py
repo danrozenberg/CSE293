eb9da94373e25666b5cc63d835aaf9934c49ec77
Dan Rozenberg <danrozenberg@gmail.com>
2016-03-31 17:24:26 -0700

    dr: add a generator to retrieve file names from folder

--- data_parser.py
+++ data_parser.py
@@ -1,11 +1,14 @@
 import os
 import csv
+import config_manager
 
+#TODO: call this data manager?
 class DataParser(object):
 
-    WORKER_EMPLOYER_BY_YEAR_FOLDER = "../data/rais/base-ano"
-
     def __init__(self):
+        config = config_manager.Config()
+
+        self.data_folder_path = config.data_path
         self.plant_from_id = {}
 
     def get_dictionary(self):
@@ -23,6 +26,27 @@ class DataParser(object):
                 DataParser.process_folder(folder_path + "/" + subfile)
 
 
+    def file_name_generator(self, folder_path, fetch_num = 0):
+        """
+        A generator...
+        Given a folder, returns names of files inside it.
+        It returns as many as fetch_num names, which makes it useful in
+        small scale tests
+        :param folder: the folder to generate file names from. Does not process
+            sub-folders
+        :param fetch_num: if 0, we will generate names until they run out,
+            otherwise, return at most fetch_num names
+        :return: the names of the files inside folder.
+        """
+        found_files = os.listdir(folder_path)
+
+        lines_read = 0
+        for subfile in found_files:
+            if os.path.isfile(folder_path + "/" +  subfile):
+                yield subfile
+                lines_read += 1
+                if 0 < fetch_num <= lines_read:
+                    break
 
     def parse_worker_factory_ano(self, file_path, fetchNum = 0):
 
--- test/test_data_parser.py
+++ test/test_data_parser.py
@@ -1,14 +1,63 @@
+from sets import Set
 import unittest
 import sys
+import config_manager
 sys.path.append('..')
 from data_parser import DataParser
 
 class TestDataParser(unittest.TestCase):
 
-    # Should initialize
-    def test_should_word(self):
-        asd = DataParser()
-        print "data parser is ok..."
+    def setUp(self):
+        self.config = config_manager.Config()
+
+    # name generator should work
+    def test_name_generator(self):
+        parser = DataParser()
+        target_folder = self.config.data_path + "base-ano/"
+
+
+        expected = Set(["ano2005.csv",
+                    "ano2006.csv",
+                    "ano2007.csv",
+                    "ano2008.csv" ])
+
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder, 0):
+            answer.add(file_name)
+        self.assertEquals(answer, expected)
+
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder):
+            answer.add(file_name)
+        self.assertEquals(answer, expected)
+
+        # listdir doesn't have a predictable order, let's just count the return numbers
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder, 1):
+            answer.add(file_name)
+        self.assertEquals(len(answer), 1)
+
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder, 2):
+            answer.add(file_name)
+        self.assertEquals(len(answer), 2)
+
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder, 3):
+            answer.add(file_name)
+        self.assertEquals(len(answer), 3)
+
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder, 4):
+            answer.add(file_name)
+        self.assertEquals(len(answer), 4)
+
+        answer = Set()
+        for file_name in parser.file_name_generator(target_folder, 100):
+            answer.add(file_name)
+        self.assertEquals(len(answer), 4)
+
+
 
 
 if __name__ == "__main__":
