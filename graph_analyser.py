646b175f2c04cf2982e52375a246b631e2c83f17
Dan Rozenberg <danrozenberg@gmail.com>
2016-04-02 11:49:23 -0700

    dr: remove networkx references

--- graph_analyser.py
+++ graph_analyser.py
@@ -1,4 +1,4 @@
-import networkx as nx
+import snap
 
 # given a graph, run a lot of analysis on it.
 
@@ -7,25 +7,10 @@ class GraphAnalyser(object):
     def __init__(self, graph):
         self.graph = graph
 
-    def draw(self, color_map = None):
-        plt = self.check_matplotlib()
-
-        # TODO: make this code better
-        if plt is not None:
-            if color_map is not None:
-                nx.draw(self.graph, node_color=[color_map[self.graph.node[node]['category']] for node in self.graph])
-            else:
-                nx.draw(self.graph)
-
-            plt.show()
-
-
-    @staticmethod
-    def check_matplotlib():
-        try:
-            import matplotlib.pyplot as plt
-            return  plt
-        except ImportError:
-            print("Matplotlib needed for drawing. Skipping")
-            return None
-
+    def draw(self):
+        # network = snap.GenRndGnm(snap.PNEANet, 10, 50)
+        #
+        # labels = snap.TIntStrH()
+        # for node in network.Nodes():
+        #     labels[node.GetId()] = str(node.GetId())
+        # snap.DrawGViz(network, snap.gvlDot, "../output_images/gvlDot.png", "MyNetwork ",labels)
