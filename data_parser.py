982f946a6eef052bd5842d865e8d9fa9a2290632
Dan Rozenberg <danrozenberg@gmail.com>
2016-04-03 12:42:44 -0700

    dr: better method names

--- data_parser.py
+++ data_parser.py
@@ -12,9 +12,6 @@ class DataParser(object):
         self.data_folder_path = config.data_path
         self.plant_from_id = {}
 
-    def get_dictionary(self):
-        return self.plant_from_id
-
     @staticmethod
     def process_folder(folder_path):
         found_files = os.listdir(folder_path)
@@ -26,9 +23,10 @@ class DataParser(object):
                 print subfile + " is a folder, diving in..."
                 DataParser.process_folder(folder_path + "/" + subfile)
 
-
     #TODO: replace with itertools
-    def file_name_generator(self, folder_path, fetch_num = 0):
+
+    @staticmethod
+    def get_file_names(folder_path, fetch_num = 0):
         """
         A generator...
         Given a folder, returns names of files inside it.
@@ -52,9 +50,7 @@ class DataParser(object):
                 if 0 < fetch_num <= names_read:
                     break
 
-    #TODO: replace with itertools
-    def file_line_reader(self, file_path, fetch_num = None):
-
+    def get_lines(self, file_path, fetch_num = None):
         #TODO: fetch more than 1 line at once on islice call.
 
         # also accepts 0 as being "all the file"
