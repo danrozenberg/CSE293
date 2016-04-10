9cc26ad0671c9a68238bfbc9478072906e6fc2a7
Dan Rozenberg <danrozenberg@gmail.com>
2016-04-09 22:26:11 -0700

    dr: parallel can now print number of lines read

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -2,10 +2,8 @@ import logging
 import data_parser
 from config_manager import Config
 
-
 from joblib import Parallel, delayed
 import multiprocessing
-
 def simple_reading():
 
     # TODO: config log somewhere else...
@@ -16,14 +14,14 @@ def simple_reading():
     lines_read = 0
     files_read = 0
     config = Config()
-
+    parser = data_parser.Pis12DataParser()
     logging.info("Start simple reading now")
 
-    for file_path in data_parser.file_paths(config.data_path, 0):
+    for file_path in parser.find_files(config.data_path, 0):
         files_read += 1
         logging.info("Started a file: " + file_path)
-        for line in data_parser.lines(file_path, 0):
-            data_parser.parse_line(line)
+        for line in parser.lines_reader(file_path, 0):
+            parser.lines_reader(line)
             lines_read += 1
 
     logging.info("Finished")
@@ -38,22 +36,29 @@ def start_parallel_reading():
     level=logging.DEBUG)
 
     config = Config()
-    files = data_parser.file_paths(config.data_path, 0)
+    parser = data_parser.Pis12DataParser()
+    files = parser.find_files(config.data_path, 0)
     num_cores = multiprocessing.cpu_count()
 
     logging.info("Start parallel reading now")
-    Parallel(n_jobs=num_cores)(delayed(parallel_reading)(f) for f in files)
+    lines_read = Parallel(n_jobs=num_cores)(delayed(parallel_reading)(f, parser) for f in files)
     logging.info("Finished")
+    logging.info("read " + str(sum(lines_read)) + " lines.")
 
-def parallel_reading(file_path):
+
+def parallel_reading(file_path, parser):
     # TODO: config log somewhere else...
     logging.basicConfig(format='%(asctime)s %(message)s',
     datefmt='%d %b - %H:%M:%S -',
     level=logging.DEBUG)
 
     logging.info("Started a file: " + file_path)
-    for line in data_parser.lines(file_path, 0):
-        data_parser.parse_line(line)
+    read_lines = 0
+    for line in parser.lines_reader(file_path, 0):
+        parser.parse_line(line)
+        read_lines += 1
+    return read_lines
+
 
 if __name__ == "__main__":
     start_parallel_reading()
