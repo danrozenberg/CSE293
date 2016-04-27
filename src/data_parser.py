6782ee4a746318c603f58d6824f892fb67958c36
Dan Rozenberg <danrozenberg@gmail.com>
2016-04-27 15:14:22 -0700

    dr: fix name mangling

--- src/data_parser.py
+++ src/data_parser.py
@@ -97,12 +97,12 @@ class Pis12DataInterpreter():
         self.__reset_private_variables()
 
     def __reset_private_variables(self):
-        self.__year = None
-        self.__admission_date = None
-        self.__demission_date = None
-        self.__time_at_employer = None
-        self.__worker_id = None
-        self.__employer_id = None
+        self._year = None
+        self._admission_date = None
+        self._demission_date = None
+        self._time_at_employer = None
+        self._worker_id = None
+        self._employer_id = None
 
     def feed_line(self, values):
         """
@@ -122,16 +122,16 @@ class Pis12DataInterpreter():
     @property
     def year(self):
         """ :return: the year the entry relates to """
-        if self.__year is None:
-            self.__year = self.__simple_retrieval('ANO', 'info')
-        return self.__year
+        if self._year is None:
+            self._year = self._simple_retrieval('ANO', 'info')
+        return self._year
 
     @property
     def admission_date(self):
         # TODO: check if tipo_adm change anything?
 
-        if self.__admission_date is not None:
-            return self.__admission_date
+        if self._admission_date is not None:
+            return self._admission_date
 
         dt_admissao = self.dict['DT_ADMISSAO']
         ano_adm = self.dict['ANO_ADM']
@@ -145,53 +145,53 @@ class Pis12DataInterpreter():
                 # add missing zeroes to day of month
                 if len(date_string) == 7:
                     date_string = '0' + str(date_string)
-                self.__admission_date = datetime.strptime(date_string,'%d%m%Y')
-                return self.__admission_date
+                self._admission_date = datetime.strptime(date_string,'%d%m%Y')
+                return self._admission_date
             except ValueError:
                 self.log_message = "Could not parse DT_ADMISSAO for: " + str(self.dict)
                 logging.warning(self.log_message)
-                self.__admission_date = -1
-                return self.__admission_date
+                self._admission_date = -1
+                return self._admission_date
 
         elif ano_adm == '' and mes_adm.isdigit():
 
             if mes_adm == '0':
                 # This happens in older records...for our purposes, let's
                 # assume that they were hired long long ago, in 1900 or something.
-                self.__admission_date = datetime(1990, 1, 1)
-                return self.__admission_date
+                self._admission_date = datetime(1800, 1, 1)
+                return self._admission_date
             else:
                 # I am assuming that in these cases, the worker was hired in the
                 # current year.
                 adm_month = int(mes_adm)
-                self.__admission_date = datetime(self.year, adm_month, 1)
-                return self.__admission_date
+                self._admission_date = datetime(self.year, adm_month, 1)
+                return self._admission_date
 
         elif ano_adm == '' and mes_adm == '0':
             # This happens in older records...for our purposes, let's
             # assume worker was hired long ago.
-            self.__admission_date = datetime(1990, 1, 1)
-            return self.__admission_date
+            self._admission_date = datetime(1800, 1, 1)
+            return self._admission_date
 
         elif ano_adm <> '' and mes_adm <> '' :
             # In this case, day dafaults to 01...
             adm_day = 1
             adm_month = int(mes_adm)
             adm_year = int(ano_adm)
-            self.__admission_date = datetime(adm_year, adm_month, adm_day)
-            return self.__admission_date
+            self._admission_date = datetime(adm_year, adm_month, adm_day)
+            return self._admission_date
         else:
             self.log_message = "could not get admission date for: " + str(self.dict)
             logging.warning(self.log_message)
-            self.__admission_date = -1
-            return self.__admission_date
+            self._admission_date = -1
+            return self._admission_date
 
     @property
     def demission_date(self):
         # TODO: explain the whole unix timestamp idea
 
-        if self.__demission_date is not None:
-            return self.__demission_date
+        if self._demission_date is not None:
+            return self._demission_date
 
         dia_desl = self.dict['DIADESL']
         mes_deslig = self.dict['MES_DESLIG']
@@ -202,8 +202,8 @@ class Pis12DataInterpreter():
                 (dia_desl == '' and mes_deslig == '0') or \
                 (dia_desl == '0' and mes_deslig == '0') or \
                 (dia_desl == 'NAO DESL ANO'):
-                    self.__demission_date =  datetime(self.year, 12, 31)
-                    return self.__demission_date
+                    self._demission_date =  datetime(self.year, 12, 31)
+                    return self._demission_date
 
             # MES_DESLIG (different from 0), but no DIADESL, let's assume that
             # the worker was let go at the first day of the month.
@@ -211,8 +211,8 @@ class Pis12DataInterpreter():
                 dem_month = int(mes_deslig)
                 dem_day = 1
                 dem_year = self.year
-                self.__demission_date = datetime(dem_year, dem_month, dem_day)
-                return self.__demission_date
+                self._demission_date = datetime(dem_year, dem_month, dem_day)
+                return self._demission_date
 
             else:
                 dem_month = int(mes_deslig)
@@ -236,18 +236,18 @@ class Pis12DataInterpreter():
                 (dem_month <= 0 < dem_day) :
                     self.log_message = "Inconsistent MES_DESLIG or DIADESL in: " + str(self.dict)
                     logging.warning(self.log_message)
-                    self.__demission_date = -1
-                    return self.__demission_date
+                    self._demission_date = -1
+                    return self._demission_date
                 else:
                     # FINALLY, we return for the normal case...
-                    self.__demission_date = datetime(dem_year, dem_month, dem_day)
-                    return self.__demission_date
+                    self._demission_date = datetime(dem_year, dem_month, dem_day)
+                    return self._demission_date
 
         except ValueError:
             self.log_message = "MES_DESLIG or DIADESL is invalid in: " + str(self.dict)
             logging.warning(self.log_message)
-            self.__demission_date = -1
-            return self.__demission_date
+            self._demission_date = -1
+            return self._demission_date
 
     @property
     def time_at_employer(self):
@@ -256,51 +256,49 @@ class Pis12DataInterpreter():
         was working for that employer.
         '''
 
-        if self.__time_at_employer is not None:
-            return self.__time_at_employer
+        if self._time_at_employer is not None:
+            return self._time_at_employer
 
         admission_date = self.admission_date
         demission_date = self.demission_date
         year = self.year
 
-        if admission_date == -1 or demission_date == -1 or year == -1:
+        # should fail if we have any invalid value, that is, not a
+        #  datetime. invalud values are set to -1
+        try:
+
+            # for time at employer, if hired before the start of the year
+            # consider only the time at employer this year
+            if admission_date < datetime(year, 1, 1):
+                admission_date = datetime(year, 1, 1)
+
+            total_days = (demission_date - admission_date).days
+            # we do not consider values greater than 365, this is because
+            # our date sources entries represent at most a time period of
+            # 1 year. So time at employer this year is capped at 365 days
+            # this cap is used when the employee was neither hired nor fired
+            # this 'year'
+            self._time_at_employer = min(365, total_days)
+            return self._time_at_employer
+        except TypeError:
             self.log_message = "Unable to calculate time_at_employer for:" + str(self.dict)
             logging.warning(self.log_message)
-            self.__time_at_employer = -1
-            return self.__time_at_employer
-
-        # pretend worker starts on Jan-1st if date is from a previous year
-        #   or if it has no admission date ( Isuppose that means he/shw was not
-        #   hired that year....)
-        if type(admission_date) == datetime and \
-           admission_date.year < year:
-            admission_date = datetime(year, 1, 1)
-        if type(admission_date) == int and admission_date == 0:
-            admission_date = datetime(year, 1, 1)
-
-
-        # if we get a 0 for demission date, we suppose the worker was not
-        #   fired that year, give it a Dec-31 date...
-        if type(demission_date) == int and demission_date == 0:
-            demission_date = datetime(year, 12, 31)
-
-        # Finally, return...
-        self.__time_at_employer = (demission_date - admission_date).days
-        return self.__time_at_employer
+            self._time_at_employer = -1
+            return self._time_at_employer
 
     @property
     def worker_id(self):
-        if self.__worker_id is None:
-            self.__worker_id = self.__simple_retrieval('PIS', 'info')
-        return self.__worker_id
+        if self._worker_id is None:
+            self._worker_id = self._simple_retrieval('PIS', 'info')
+        return self._worker_id
 
     @property
     def employer_id(self):
-        if self.__employer_id is None:
-            self.__employer_id = self.__simple_retrieval('IDENTIFICAD', 'info')
-        return self.__employer_id
+        if self._employer_id is None:
+            self._employer_id = self._simple_retrieval('IDENTIFICAD', 'info')
+        return self._employer_id
 
-    def __simple_retrieval(self, field_name, alert_level='warn'):
+    def _simple_retrieval(self, field_name, alert_level='warn'):
         """
         for data that is straightforward to get.
         :return: an integer
