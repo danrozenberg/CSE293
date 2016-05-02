29c0a5e7fb9373ffe7f9e1e68dd193236f7c013f
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-02 13:10:12 -0700

    dr: remove old code, clean up a bit

--- src/connect_workers.py
+++ src/connect_workers.py
@@ -8,50 +8,6 @@ def get_worker_iterator(association_graph):
         if node_type == "worker":
             yield node
 
-def connect_workers(association_graph, new_graph):
-    """
-    Connects workers as long as they have worked in the same company
-      at any given point, including if they never worked together
-      during the same period of time. This is the most simple way to
-      connect the workers.
-
-    :param association_graph: an association graph between workers-plants.
-    We expect this association graph to have ONLY ONE EDGE BETWEEN ANY
-     PAIR OF NODES.
-
-    :param graph_manager: a graph manager that we will use to create a
-     new graph.
-    :return: the new graph, with only workers and edges between them.
-    """
-
-    # we get a special worker iterator
-    for worker in get_worker_iterator(association_graph):
-
-        # add this worker to the new graph
-        association_graph.copy_node(worker, new_graph)
-
-        # In an association graph, we can get the employers just by
-        # following the edges from worker and retrieving the neighbors.
-        employer_nodes = association_graph.get_neighboring_nodes(worker)
-
-        # We don't need to consider the worker node or its edges again.
-        #  so we could delete it...
-        # Its kind of a bad vibe to delete the node inside a node iterator
-        #   just make sure that iterator can deal with it and that
-        #   when you delete a node the manager will delete the edges..
-        association_graph.delete_node(worker)
-
-        for employer in employer_nodes:
-            for coworker in association_graph.get_neighboring_nodes(employer):
-                new_graph.add_node(coworker)
-
-                # don't want repeated edges. This could happen if
-                # worker and coworker worked together in 2 different plants.
-                if not new_graph.is_edge_between(worker, coworker):
-                    new_graph.add_edge(worker, coworker)
-
-    return new_graph
-
 
 def get_time_together(worker_edge, coworker_edge, graph):
     time_together = 0
@@ -69,63 +25,6 @@ def get_time_together(worker_edge, coworker_edge, graph):
                 coworker_edge_attrs[admission_string],
                 coworker_edge_attrs[demission_string])
     return time_together
-
-
-class SimpleConnector(object):
-    """ Connects workers if they have worked together
-    at any point in time.
-    """
-
-    def connect_workers(self, association_graph, new_graph):
-        # we get a special worker iterator
-        for worker in get_worker_iterator(association_graph):
-
-            # add this worker to the new graph, if necessary
-            association_graph.copy_node(worker, new_graph)
-
-            # In an association graph, we can get the employers just by
-            # following the edges from worker and retrieving the neighbors.
-            employer_nodes = association_graph.get_neighboring_nodes(worker)
-
-            for employer in employer_nodes:
-                worker_edge = association_graph.get_edge_between(worker, employer)
-
-                for coworker in association_graph.get_neighboring_nodes(employer):
-                    # skip if worker and coworkers are the same
-
-                    if worker == coworker:
-                        continue
-
-                    coworker_edge = association_graph.get_edge_between(coworker, employer)
-
-                    if worker == 2 and  coworker == 9:
-                        print coworker_edge
-                        print self.should_connect(worker_edge, coworker_edge, association_graph)
-
-                    if self.should_connect(worker_edge, coworker_edge, association_graph):
-                        new_graph.add_node(coworker)
-
-                        if not new_graph.is_edge_between(worker, coworker):
-                            new_graph.add_edge(worker, coworker)
-                            # TODO: maybe put time together in the attr?
-                            # TODO: maybe put in some other attrs?
-
-        return new_graph
-
-
-
-    def should_connect(self, worker_edge, coworker_edge, graph):
-        """
-        :param worker_edge: id edge connecting worker to employee
-        :param coworker_edge: id of edge connecting employee to a
-            different worker.
-        :return: wether we should add an edge between them.
-        """
-
-        time_together = get_time_together(worker_edge,
-                                               coworker_edge,
-                                               graph)
-        return time_together > 1
 
 
 def get_overlapping_days(start_1, end_1, start_2, end_2):
@@ -146,6 +45,61 @@ def get_overlapping_days(start_1, end_1, start_2, end_2):
     latest_start = max(r1.start, r2.start)
     earliest_end = min(r1.end, r2.end)
     return max((earliest_end - latest_start).days + 1, 0)
+
+
+class WorkerConnector(object):
+    def __init__(self):
+        # defaults allows workers with 0 days in common to be connected.
+        self.min_days_together = -1
+
+    def connect_workers(self, association_graph, new_graph):
+        # we get a special worker iterator
+        for worker in get_worker_iterator(association_graph):
+
+            # add this worker to the new graph, if necessary
+            association_graph.copy_node(worker, new_graph)
+
+            # In an association graph, we can get the employers just by
+            # following the edges from worker and retrieving the neighbors.
+            employer_nodes = association_graph.get_neighboring_nodes(worker)
+
+            for employer in employer_nodes:
+                worker_edge = association_graph.get_edge_between(worker, employer)
+
+                for coworker in association_graph.get_neighboring_nodes(employer):
+
+                    # no need to connect someone with oneself...
+                    if worker == coworker:
+                        continue
+
+                    coworker_edge = association_graph.get_edge_between(coworker, employer)
+
+                    if self.should_connect(worker_edge, coworker_edge, association_graph):
+                        new_graph.add_node(coworker)
+
+                        if not new_graph.is_edge_between(worker, coworker):
+                            new_graph.add_edge(worker, coworker)
+                            # TODO: maybe put time together in the attr?
+                            # TODO: maybe put in some other attrs?
+
+        return new_graph
+
+    def should_connect(self, worker_edge, coworker_edge, graph):
+        """
+        :param worker_edge: id edge connecting worker to employee
+        :param coworker_edge: id of edge connecting employee to a
+            different worker.
+        :return: wether we should add an edge between them.
+        """
+
+        time_together = get_time_together(worker_edge,
+                                               coworker_edge,
+                                               graph)
+
+        # add more checks here, as needed.
+        return time_together >= self.min_days_together
+
+
 
 
 
--- test/test_connect_workers.py
+++ test/test_connect_workers.py
@@ -7,80 +7,11 @@ sys.path.insert(0, '../src/')
 import graph_manager
 import connect_workers
 
-
-class ConnectWorkersTest(unittest.TestCase):
-
+class WorkerConnector(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(format='%(asctime)s %(message)s',
         datefmt='%d %b - %H:%M:%S -',
         level=logging.INFO)
-
-    def test_get_worker_iterator(self):
-        manager = graph_manager.SnapManager()
-
-        # add a few nodes
-        manager.add_node(10)
-        manager.add_node(20)
-        manager.add_node(30)
-        manager.add_node(40)
-        manager.add_node(50)
-        manager.add_node(60)
-        manager.add_node(70)
-
-        # make some worker nodes, others as employer nodes
-        manager.add_node_attr(10, "type", "worker")
-        manager.add_node_attr(20, "type", "employer")
-        manager.add_node_attr(30, "type", "worker")
-        manager.add_node_attr(40, "type", "employer")
-        manager.add_node_attr(50, "type", "worker")
-        manager.add_node_attr(60, "type", "employer")
-        manager.add_node_attr(70, "type", "worker")
-
-        # get iterator
-        it = connect_workers.get_worker_iterator(manager)
-
-        # test it
-        found_ids = []
-        for node in it:
-            self.assertEquals("worker",
-                              manager.get_node_attr(node, "type"))
-            found_ids.append(node)
-
-        self.assertListEqual([10,30,50,70], found_ids)
-
-    def test_connect_workers(self):
-        manager = graph_manager.SnapManager()
-        self.create_association_graph(manager)
-
-        new_graph = graph_manager.SnapManager()
-        connect_workers.connect_workers(manager,new_graph)
-
-        # Graph should have 9 nodes only
-        self.assertEqual(9, new_graph.get_node_count())
-
-        # All edges that should exist
-        self.assertTrue(new_graph.is_edge_between(1,2))
-        self.assertTrue(new_graph.is_edge_between(1,3))
-        self.assertTrue(new_graph.is_edge_between(2,3))
-        self.assertTrue(new_graph.is_edge_between(9,1))
-        self.assertTrue(new_graph.is_edge_between(9,2))
-        self.assertTrue(new_graph.is_edge_between(9,3))
-        self.assertTrue(new_graph.is_edge_between(9,4))
-        self.assertTrue(new_graph.is_edge_between(9,5))
-        self.assertTrue(new_graph.is_edge_between(4,7))
-        self.assertTrue(new_graph.is_edge_between(4,6))
-        self.assertTrue(new_graph.is_edge_between(4,5))
-        self.assertTrue(new_graph.is_edge_between(5,6))
-        self.assertTrue(new_graph.is_edge_between(5,7))
-        self.assertTrue(new_graph.is_edge_between(6,9))
-
-        # some edges that should not exist
-        self.assertFalse(new_graph.is_edge_between(3,7))
-        self.assertFalse(new_graph.is_edge_between(7,7))
-        self.assertFalse(new_graph.is_edge_between(9,8))
-        self.assertFalse(new_graph.is_edge_between(9,7))
-        self.assertFalse(new_graph.is_edge_between(1,7))
-        self.assertFalse(new_graph.is_edge_between(1,5))
 
     def test_get_overlapping_days(self):
         start_1 =  mktime(datetime(2010,1,1).timetuple())
@@ -138,58 +69,40 @@ class ConnectWorkersTest(unittest.TestCase):
                                                                start_2,
                                                                end_2))
 
-    def create_association_graph(self, manager):
-        # 9 workers
-        manager.add_node(1)
-        manager.add_node(2)
-        manager.add_node(3)
-        manager.add_node(4)
-        manager.add_node(5)
-        manager.add_node(6)
-        manager.add_node(7)
-        manager.add_node(8)
-        manager.add_node(9)
-        manager.add_node_attr(1, "type", "worker")
-        manager.add_node_attr(2, "type", "worker")
-        manager.add_node_attr(3, "type", "worker")
-        manager.add_node_attr(4, "type", "worker")
-        manager.add_node_attr(5, "type", "worker")
-        manager.add_node_attr(6, "type", "worker")
-        manager.add_node_attr(7, "type", "worker")
-        manager.add_node_attr(8, "type", "worker")
-        manager.add_node_attr(9, "type", "worker")
+    def test_get_worker_iterator(self):
+        manager = graph_manager.SnapManager()
 
-        # 3 plants
+        # add a few nodes
         manager.add_node(10)
         manager.add_node(20)
         manager.add_node(30)
-        manager.add_node_attr(10, "type", "employer")
+        manager.add_node(40)
+        manager.add_node(50)
+        manager.add_node(60)
+        manager.add_node(70)
+
+        # make some worker nodes, others as employer nodes
+        manager.add_node_attr(10, "type", "worker")
         manager.add_node_attr(20, "type", "employer")
-        manager.add_node_attr(30, "type", "employer")
+        manager.add_node_attr(30, "type", "worker")
+        manager.add_node_attr(40, "type", "employer")
+        manager.add_node_attr(50, "type", "worker")
+        manager.add_node_attr(60, "type", "employer")
+        manager.add_node_attr(70, "type", "worker")
 
-        # connect them
-        manager.add_edge(1, 10)
-        manager.add_edge(2, 10)
-        manager.add_edge(3, 10)
-        manager.add_edge(9, 10)
+        # get iterator
+        it = connect_workers.get_worker_iterator(manager)
 
-        manager.add_edge(4, 20)
-        manager.add_edge(5, 20)
-        manager.add_edge(6, 20)
-        manager.add_edge(9, 20)
+        # test it
+        found_ids = []
+        for node in it:
+            self.assertEquals("worker",
+                              manager.get_node_attr(node, "type"))
+            found_ids.append(node)
 
-        manager.add_edge(4, 30)
-        manager.add_edge(5, 30)
-        manager.add_edge(7, 30)
-
-class SimpleConnectorTest(unittest.TestCase):
-    def setUp(self):
-        logging.basicConfig(format='%(asctime)s %(message)s',
-        datefmt='%d %b - %H:%M:%S -',
-        level=logging.INFO)
+        self.assertListEqual([10,30,50,70], found_ids)
 
     def test_get_time_at_worker_attrs(self):
-        connector = connect_workers.SimpleConnector()
         manager = graph_manager.SnapManager()
 
         # create 2 workers, 1 employee node
@@ -258,12 +171,13 @@ class SimpleConnectorTest(unittest.TestCase):
                                                           manager)
         self.assertEquals(78, time_together)
 
-    def test_connect_workers(self):
+    def test_connect_workers_with_min_days(self):
         manager = graph_manager.SnapManager()
         self.create_association_graph(manager)
 
         new_graph = graph_manager.SnapManager()
-        connector = connect_workers.SimpleConnector()
+        connector = connect_workers.WorkerConnector()
+        connector.min_days_together = 1
         connector.connect_workers(manager, new_graph)
 
         # Graph should have 9 nodes only
@@ -293,6 +207,40 @@ class SimpleConnectorTest(unittest.TestCase):
         self.assertFalse(new_graph.is_edge_between(1,9))
         self.assertFalse(new_graph.is_edge_between(9,1))
 
+    def test_connect_workers_no_min_days(self):
+        manager = graph_manager.SnapManager()
+        self.create_association_graph(manager)
+
+        new_graph = graph_manager.SnapManager()
+        connector = connect_workers.WorkerConnector()
+        connector.connect_workers(manager,new_graph)
+
+        # Graph should have 9 nodes only
+        self.assertEqual(9, new_graph.get_node_count())
+
+        # All edges that should exist
+        self.assertTrue(new_graph.is_edge_between(1,2))
+        self.assertTrue(new_graph.is_edge_between(1,3))
+        self.assertTrue(new_graph.is_edge_between(2,3))
+        self.assertTrue(new_graph.is_edge_between(9,1))
+        self.assertTrue(new_graph.is_edge_between(9,2))
+        self.assertTrue(new_graph.is_edge_between(9,3))
+        self.assertTrue(new_graph.is_edge_between(9,4))
+        self.assertTrue(new_graph.is_edge_between(9,5))
+        self.assertTrue(new_graph.is_edge_between(4,7))
+        self.assertTrue(new_graph.is_edge_between(4,6))
+        self.assertTrue(new_graph.is_edge_between(4,5))
+        self.assertTrue(new_graph.is_edge_between(5,6))
+        self.assertTrue(new_graph.is_edge_between(5,7))
+        self.assertTrue(new_graph.is_edge_between(6,9))
+
+        # some edges that should not exist
+        self.assertFalse(new_graph.is_edge_between(3,7))
+        self.assertFalse(new_graph.is_edge_between(7,7))
+        self.assertFalse(new_graph.is_edge_between(9,8))
+        self.assertFalse(new_graph.is_edge_between(9,7))
+        self.assertFalse(new_graph.is_edge_between(1,7))
+        self.assertFalse(new_graph.is_edge_between(1,5))
 
     def create_association_graph(self, manager):
         # 9 workers
