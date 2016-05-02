81fa52d4523845602238b89ebbdd708044582492
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-02 12:51:04 -0700

    dr: connect workers if they ever worked together

--- src/connect_workers.py
+++ src/connect_workers.py
@@ -4,11 +4,11 @@ from collections import namedtuple
 def get_worker_iterator(association_graph):
     node_iterator = association_graph.get_node_iterator()
     for node in node_iterator:
-        node_type = association_graph.get_node_attribute(node, "type")
+        node_type = association_graph.get_node_attr(node, "type")
         if node_type == "worker":
             yield node
 
-def connect_workers(association_graph, new_graph, connector):
+def connect_workers(association_graph, new_graph):
     """
     Connects workers as long as they have worked in the same company
       at any given point, including if they never worked together
@@ -52,46 +52,100 @@ def connect_workers(association_graph, new_graph, connector):
 
     return new_graph
 
-def get_overlapping_days(start_1, end_1, start_2, end_2):
-    # from https://stackoverflow.com/questions/9044084/efficient-date-range-overlap-calculation-in-python
 
-    fromtimestamp = datetime.fromtimestamp
+def get_time_together(worker_edge, coworker_edge, graph):
+    time_together = 0
+    worker_edge_attrs = graph.get_edge_attrs(worker_edge)
+    coworker_edge_attrs = graph.get_edge_attrs(coworker_edge)
+    for year in xrange(1980, 2030):
+        admission_string = str(year) + "_admission_date"
+        demission_string = str(year) + "_demission_date"
 
-    Range = namedtuple('Range', ['start', 'end'])
-    r1 = Range(start=fromtimestamp(start_1), end=fromtimestamp(end_1))
-    r2 = Range(start=fromtimestamp(start_2), end=fromtimestamp(end_2))
-    latest_start = max(r1.start, r2.start)
-    earliest_end = min(r1.end, r2.end)
-    return max((earliest_end - latest_start).days + 1, 0)
-
-class WorkerConnector():
-    def should_connect(self, worker, coworker):
-        raise NotImplementedError
+        if (admission_string in worker_edge_attrs) and \
+           (admission_string in coworker_edge_attrs):
+            time_together += get_overlapping_days(
+                worker_edge_attrs[admission_string],
+                worker_edge_attrs[demission_string],
+                coworker_edge_attrs[admission_string],
+                coworker_edge_attrs[demission_string])
+    return time_together
 
 
-class SimpleConnector(WorkerConnector):
+class SimpleConnector(object):
     """ Connects workers if they have worked together
     at any point in time.
     """
-    def __init__(self, graph):
-        self.graph = graph
 
-    def should_connect(self, worker, coworker):
+    def connect_workers(self, association_graph, new_graph):
+        # we get a special worker iterator
+        for worker in get_worker_iterator(association_graph):
+
+            # add this worker to the new graph, if necessary
+            association_graph.copy_node(worker, new_graph)
+
+            # In an association graph, we can get the employers just by
+            # following the edges from worker and retrieving the neighbors.
+            employer_nodes = association_graph.get_neighboring_nodes(worker)
+
+            for employer in employer_nodes:
+                worker_edge = association_graph.get_edge_between(worker, employer)
+
+                for coworker in association_graph.get_neighboring_nodes(employer):
+                    # skip if worker and coworkers are the same
+
+                    if worker == coworker:
+                        continue
+
+                    coworker_edge = association_graph.get_edge_between(coworker, employer)
+
+                    if worker == 2 and  coworker == 9:
+                        print coworker_edge
+                        print self.should_connect(worker_edge, coworker_edge, association_graph)
+
+                    if self.should_connect(worker_edge, coworker_edge, association_graph):
+                        new_graph.add_node(coworker)
+
+                        if not new_graph.is_edge_between(worker, coworker):
+                            new_graph.add_edge(worker, coworker)
+                            # TODO: maybe put time together in the attr?
+                            # TODO: maybe put in some other attrs?
+
+        return new_graph
+
+
+
+    def should_connect(self, worker_edge, coworker_edge, graph):
         """
-        :param worker: id of a worker
-        :param coworker: id of another worker
+        :param worker_edge: id edge connecting worker to employee
+        :param coworker_edge: id of edge connecting employee to a
+            different worker.
         :return: wether we should add an edge between them.
         """
-        pass
+
+        time_together = get_time_together(worker_edge,
+                                               coworker_edge,
+                                               graph)
+        return time_together > 1
 
 
+def get_overlapping_days(start_1, end_1, start_2, end_2):
+    # from https://stackoverflow.com/questions/9044084/efficient-date-range-overlap-calculation-in-python
 
+    # convert to datetime if we received timestamp
+    # also, assumes all parameters are in the same format
+    if type(start_1) == float:
+        fromtimestamp = datetime.fromtimestamp
+        start_1 = fromtimestamp(start_1)
+        end_1 = fromtimestamp(end_1)
+        start_2 = fromtimestamp(start_2)
+        end_2 = fromtimestamp(end_2)
 
-    def _get_time_at_worker_attrs(self, node_id):
-        pass
-
-
-
+    Range = namedtuple('Range', ['start', 'end'])
+    r1 = Range(start=start_1, end=end_1)
+    r2 = Range(start=start_2, end=end_2)
+    latest_start = max(r1.start, r2.start)
+    earliest_end = min(r1.end, r2.end)
+    return max((earliest_end - latest_start).days + 1, 0)
 
 
 
--- test/test_connect_workers.py
+++ test/test_connect_workers.py
@@ -43,7 +43,7 @@ class ConnectWorkersTest(unittest.TestCase):
         found_ids = []
         for node in it:
             self.assertEquals("worker",
-                              manager.get_node_attribute(node, "type"))
+                              manager.get_node_attr(node, "type"))
             found_ids.append(node)
 
         self.assertListEqual([10,30,50,70], found_ids)
@@ -92,10 +92,11 @@ class ConnectWorkersTest(unittest.TestCase):
                                                                    start_2,
                                                                    end_2))
 
-        start_2 = mktime(datetime(2010,1,1).timetuple())
-        end_2 = mktime(datetime(2015,1,1).timetuple())
-        start_1 = mktime(datetime(2011,10,10).timetuple())
-        end_1 = mktime(datetime(2011,10,20).timetuple())
+        # we can pass datetimes or timestamps :)
+        start_2 = datetime(2010,1,1)
+        end_2 = datetime(2015,1,1)
+        start_1 = datetime(2011,10,10)
+        end_1 = datetime(2011,10,20)
         self.assertEquals(11, connect_workers.get_overlapping_days(start_1,
                                                                end_1,
                                                                start_2,
@@ -136,7 +137,6 @@ class ConnectWorkersTest(unittest.TestCase):
                                                                end_1,
                                                                start_2,
                                                                end_2))
-
 
     def create_association_graph(self, manager):
         # 9 workers
@@ -190,9 +190,109 @@ class SimpleConnectorTest(unittest.TestCase):
 
     def test_get_time_at_worker_attrs(self):
         connector = connect_workers.SimpleConnector()
+        manager = graph_manager.SnapManager()
 
+        # create 2 workers, 1 employee node
+        manager.add_node(1)
+        manager.add_node(2)
+        manager.add_node(888) # the employer
+
+        # edge with no info
+        worker_edge = 10
+        coworker_edge = 20
+        manager.add_edge(1,888, worker_edge)
+        manager.add_edge(2,888, coworker_edge)
+
+        # Nothing so far
+        time_together = connect_workers.get_time_together(worker_edge,
+                                                          coworker_edge,
+                                                          manager)
+        self.assertEquals(0, time_together)
+
+        #lets say they worked together for 19 days in 2013
+        manager.add_edge_attr(worker_edge,
+                              "2013_admission_date",
+                              mktime(datetime(2013,1,1).timetuple()))
+        manager.add_edge_attr(worker_edge,
+                              "2013_demission_date",
+                              mktime(datetime(2013,12,31).timetuple()))
+
+        manager.add_edge_attr(coworker_edge,
+                              "2013_admission_date",
+                              mktime(datetime(2013,4,1).timetuple()))
+        manager.add_edge_attr(coworker_edge,
+                              "2013_demission_date",
+                              mktime(datetime(2013,4,20).timetuple()))
+        time_together = connect_workers.get_time_together(worker_edge,
+                                                          coworker_edge,
+                                                          manager)
+        self.assertEquals(19, time_together)
+
+
+        #lets say they didn't work together in 2014
+        manager.add_edge_attr(worker_edge,
+                              "2014_admission_date",
+                              mktime(datetime(2013,1,1).timetuple()))
+        manager.add_edge_attr(worker_edge,
+                              "2014_demission_date",
+                              mktime(datetime(2013,12,31).timetuple()))
+
+        self.assertEquals(19, time_together)
+
+        #worked more together in 2015
+        manager.add_edge_attr(worker_edge,
+                              "2015_admission_date",
+                              mktime(datetime(2015,1,1).timetuple()))
+        manager.add_edge_attr(worker_edge,
+                              "2015_demission_date",
+                              mktime(datetime(2015,3,31).timetuple()))
+
+        manager.add_edge_attr(coworker_edge,
+                              "2015_admission_date",
+                              mktime(datetime(2015,2,1).timetuple()))
+        manager.add_edge_attr(coworker_edge,
+                              "2015_demission_date",
+                              mktime(datetime(2015,12,31).timetuple()))
+        time_together = connect_workers.get_time_together(worker_edge,
+                                                          coworker_edge,
+                                                          manager)
+        self.assertEquals(78, time_together)
+
+    def test_connect_workers(self):
         manager = graph_manager.SnapManager()
         self.create_association_graph(manager)
+
+        new_graph = graph_manager.SnapManager()
+        connector = connect_workers.SimpleConnector()
+        connector.connect_workers(manager, new_graph)
+
+        # Graph should have 9 nodes only
+        self.assertEqual(9, new_graph.get_node_count())
+
+        # All edges that should exist
+        self.assertTrue(new_graph.is_edge_between(1,2))
+        self.assertTrue(new_graph.is_edge_between(1,3))
+        self.assertTrue(new_graph.is_edge_between(9,2))
+        self.assertTrue(new_graph.is_edge_between(9,4))
+        self.assertTrue(new_graph.is_edge_between(5,4))
+        self.assertTrue(new_graph.is_edge_between(5,6))
+        self.assertTrue(new_graph.is_edge_between(5,7))
+
+        # some edges that should not exist
+        self.assertFalse(new_graph.is_edge_between(7,7))
+        self.assertFalse(new_graph.is_edge_between(9,8))
+        self.assertFalse(new_graph.is_edge_between(9,7))
+        self.assertFalse(new_graph.is_edge_between(1,7))
+        self.assertFalse(new_graph.is_edge_between(1,5))
+
+        # worked together, but not at the same time
+        self.assertFalse(new_graph.is_edge_between(2,3))
+        self.assertFalse(new_graph.is_edge_between(4,6))
+        self.assertFalse(new_graph.is_edge_between(7,4))
+        self.assertFalse(new_graph.is_edge_between(9,5))
+        self.assertFalse(new_graph.is_edge_between(1,9))
+        self.assertFalse(new_graph.is_edge_between(9,1))
+
 
     def create_association_graph(self, manager):
         # 9 workers
@@ -224,20 +324,100 @@ class SimpleConnectorTest(unittest.TestCase):
         manager.add_node_attr(30, "type", "employer")
 
         # connect them
-        manager.add_edge(1, 10)
-        manager.add_edge(2, 10)
-        manager.add_edge(3, 10)
-        manager.add_edge(9, 10)
+        manager.add_edge(1, 10, 100)
+        manager.add_edge(2, 10, 200)
+        manager.add_edge(3, 10, 300)
+        manager.add_edge(9, 10, 400)
 
-        manager.add_edge(4, 20)
-        manager.add_edge(5, 20)
-        manager.add_edge(6, 20)
-        manager.add_edge(9, 20)
+        manager.add_edge(4, 20, 500)
+        manager.add_edge(5, 20, 600)
+        manager.add_edge(6, 20, 700)
+        manager.add_edge(9, 20, 800)
 
-        manager.add_edge(4, 30)
-        manager.add_edge(5, 30)
-        manager.add_edge(7, 30)
+        manager.add_edge(4, 30, 900)
+        manager.add_edge(5, 30, 1000)
+        manager.add_edge(7, 30, 1100)
 
+
+        # 1 worked with 3 and 2
+        manager.add_edge_attr(100, "1999_admission_date",
+                              mktime(datetime(1999,1,1).timetuple()))
+        manager.add_edge_attr(100, "1999_demission_date",
+                              mktime(datetime(1999,12,31).timetuple()))
+        manager.add_edge_attr(100, "2000_admission_date",
+                              mktime(datetime(2000,1,1).timetuple()))
+        manager.add_edge_attr(100, "2000_demission_date",
+                              mktime(datetime(2000,12,31).timetuple()))
+        manager.add_edge_attr(100, "2001_admission_date",
+                              mktime(datetime(2001,1,1).timetuple()))
+        manager.add_edge_attr(100, "2001_demission_date",
+                              mktime(datetime(2001,12,31).timetuple()))
+
+        manager.add_edge_attr(200, "1999_admission_date",
+                              mktime(datetime(1999,5,5).timetuple()))
+        manager.add_edge_attr(200, "1999_demission_date",
+                              mktime(datetime(1999,12,31).timetuple()))
+
+        manager.add_edge_attr(300, "2001_admission_date",
+                              mktime(datetime(2001,6,6).timetuple()))
+        manager.add_edge_attr(300, "2001_demission_date",
+                              mktime(datetime(2001,12,31).timetuple()))
+
+        # 2 worked with 9
+        manager.add_edge_attr(200, "2002_admission_date",
+                              mktime(datetime(2002,5,5).timetuple()))
+        manager.add_edge_attr(200, "2002_demission_date",
+                              mktime(datetime(2002,12,31).timetuple()))
+        manager.add_edge_attr(400, "2002_admission_date",
+                              mktime(datetime(2002,1,1).timetuple()))
+        manager.add_edge_attr(400, "2002_demission_date",
+                              mktime(datetime(2002,6,1).timetuple()))
+
+        # 9 worked with 4
+        manager.add_edge_attr(800, "2003_admission_date",
+                              mktime(datetime(2003,1,1).timetuple()))
+        manager.add_edge_attr(800, "2003_demission_date",
+                              mktime(datetime(2003,6,1).timetuple()))
+        manager.add_edge_attr(800, "2004_admission_date",
+                              mktime(datetime(2004,1,1).timetuple()))
+        manager.add_edge_attr(800, "2004_demission_date",
+                              mktime(datetime(2004,6,1).timetuple()))
+
+        manager.add_edge_attr(500, "2004_admission_date",
+                              mktime(datetime(2004,1,1).timetuple()))
+        manager.add_edge_attr(500, "2004_demission_date",
+                              mktime(datetime(2004,10,31).timetuple()))
+
+
+        # 4 worked with 5
+        manager.add_edge_attr(600, "2004_admission_date",
+                              mktime(datetime(2004,7,1).timetuple()))
+        manager.add_edge_attr(600, "2004_demission_date",
+                              mktime(datetime(2004,12,31).timetuple()))
+
+        # 5 worked with 6
+        manager.add_edge_attr(700, "2004_admission_date",
+                              mktime(datetime(2004,11,1).timetuple()))
+        manager.add_edge_attr(700, "2004_demission_date",
+                              mktime(datetime(2004,12,25).timetuple()))
+
+        # 5 worked with 7
+        manager.add_edge_attr(1000, "2005_admission_date",
+                              mktime(datetime(2005,1,1).timetuple()))
+        manager.add_edge_attr(1000, "2005_demission_date",
+                              mktime(datetime(2005,7,1).timetuple()))
+
+        manager.add_edge_attr(1100, "2005_admission_date",
+                              mktime(datetime(2005,1,1).timetuple()))
+        manager.add_edge_attr(1100, "2005_demission_date",
+                              mktime(datetime(2005,6,1).timetuple()))
+
+        # try to trick the algorithm
+        # 4 will work with 5 again, but in a different company
+        manager.add_edge_attr(900, "2005_admission_date",
+                              mktime(datetime(2005,7,1).timetuple()))
+        manager.add_edge_attr(900, "2005_demission_date",
+                              mktime(datetime(2005,7,1).timetuple()))
 
 if __name__ == "__main__":
     unittest.main()
