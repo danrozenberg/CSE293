932f0147cc203d066bf056120bfc84c6aece810c
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-06 16:42:00 -0700

    dr: create a statistics gatherer

--- src/statistics_gatherer.py
+++ src/statistics_gatherer.py
@@ -1,1 +1,22 @@
-__author__ = 'dan'
+# gathers and prints statistics
+
+class SnapStatisticsGatherer(object):
+    def __init__(self, graph):
+        self.graph = graph
+
+    # generate a report on the graph.
+    def report(self):
+        pass
+
+    def fill_results(self, node_list, graph_method):
+        print "asd"
+        statistics = []
+        for node in node_list:
+            statistics.append(graph_method(node))
+            
+        return statistics
+
+    def save_report(self, output_path):
+        # saves output in a csv, ready to be EXCELED!
+        pass
+
--- test/test_statistics_gatherer.py
+++ test/test_statistics_gatherer.py
@@ -1,1 +1,35 @@
-__author__ = 'dan'
+import unittest
+import sys
+sys.path.insert(0, '../src/')
+from statistics_gatherer import SnapStatisticsGatherer
+import graph_manager
+
+
+class TestSnapStatisticsGatherer(unittest.TestCase):
+
+    def test_init(self):
+        # should initialize with no errors
+        manager = graph_manager.SnapManager()
+        gatherer = SnapStatisticsGatherer(manager)
+        self.assertIsNotNone(gatherer)
+
+    def test_fill_results(self):
+        manager = graph_manager.SnapManager()
+        gatherer = SnapStatisticsGatherer(manager)
+
+        f = lambda x: x * 10
+
+        # do nothing
+        results =  gatherer.fill_results([], f)
+        self.assertEquals([], results)
+
+        # do something
+        results =  gatherer.fill_results([5,7,8,9], f)
+        self.assertEquals([50,70,80,90], results)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
