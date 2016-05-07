c1952466e876286f73bf64f5d67400d1ffe19650
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-07 10:13:12 -0700

    dr: add histogram builder

--- src/statistics_gatherer.py
+++ src/statistics_gatherer.py
@@ -1,3 +1,5 @@
+import math
+import matplotlib.pyplot as plt
 
 class StatisticsGatherer(object):
 
@@ -9,6 +11,22 @@ class StatisticsGatherer(object):
     @staticmethod
     def fill_results(node_list, graph_method):
         return map(graph_method, node_list)
+
+
+    @staticmethod
+    def build_histogram(result_list, save_path, x_label="Value", y_label="Frequency", title=""):
+        bin_num = len(set(result_list))
+        plt.hist(result_list, bin_num, normed=1, facecolor='g', alpha=0.75)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title)
+        plt.axis([min(result_list), max(result_list), 0, 1])
+
+        # for degugging how it looks
+        # plt.show()
+
+        # save
+        plt.savefig(save_path)
 
     def save_report(self, output_path):
         # saves output in a csv, ready to be EXCELED!
