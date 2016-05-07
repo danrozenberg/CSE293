264f462e3f8bc800cfbbf30d81b65fb0d2d7f8c5
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-07 10:15:48 -0700

    dr: add test for saving histogram

--- test/test_statistics_gatherer.py
+++ test/test_statistics_gatherer.py
@@ -1,22 +1,15 @@
+import os
 import unittest
 import sys
 sys.path.insert(0, '../src/')
-from statistics_gatherer import SnapStatisticsGatherer
+from statistics_gatherer import StatisticsGatherer
 import graph_manager
 
 
 class TestSnapStatisticsGatherer(unittest.TestCase):
 
-    def test_init(self):
-        # should initialize with no errors
-        manager = graph_manager.SnapManager()
-        gatherer = SnapStatisticsGatherer(manager)
-        self.assertIsNotNone(gatherer)
-
     def test_fill_results(self):
-        manager = graph_manager.SnapManager()
-        gatherer = SnapStatisticsGatherer(manager)
-
+        gatherer = StatisticsGatherer
         f = lambda x: x * 10
 
         # do nothing
@@ -27,8 +20,22 @@ class TestSnapStatisticsGatherer(unittest.TestCase):
         results =  gatherer.fill_results([5,7,8,9], f)
         self.assertEquals([50,70,80,90], results)
 
+    def test_historgram(self):
+        gatherer = StatisticsGatherer
+        manager = graph_manager.SnapManager()
+        manager.generate_random_graph(20,2,0.5)
+        node_list = range(20)
+        method = manager.get_shortest_path_size
 
+        results = gatherer.fill_results(node_list, method)
+        file_path = "./test.jpg"
+        gatherer.build_histogram(results, file_path)
 
+        # file is saved
+        self.assertTrue(os.path.isfile(file_path))
+
+        # cleanup
+        os.remove(file_path)
 
 
 if __name__ == "__main__":
