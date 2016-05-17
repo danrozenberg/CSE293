19e92e5052fb5808ae8b3a269f62096c40e0d66d
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-17 09:30:09 -0700

    dr: cythonize both files, fix setup

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(
-ext_modules = cythonize("connect_workers.pyx")
-)
+setup(ext_modules = cythonize("graph_manager.pyx"))
+setup(ext_modules = cythonize("connect_workers.pyx"))
+
