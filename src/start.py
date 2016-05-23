10b5b0ac5d06b325079e6f6e58d355a869888a29
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-23 10:50:12 -0700

    dr: remove old method calls

--- src/start.py
+++ src/start.py
@@ -1,16 +1,10 @@
 import logging
 import sys
 import subprocess
-import connect_workers
-
 
 logging.basicConfig(format='%(asctime)s %(message)s',
 datefmt='%d %b - %H:%M:%S -',
 level=logging.WARN)
-
-logging.warn("Clearing edges output folder")
-output_folder = "../output_edges/"
-connect_workers.delete_files(output_folder)
 
 procs = []
 first_year = 1994
