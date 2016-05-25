d2a9f685a7ebe64faba79b5a4e9fb32778a16fe2
Dan Rozenberg <ranrozenberg@hadoop.internal>
2016-05-25 11:10:16 -0700

    dr: remove matplotlib, some hardcoding of paths

--- src/statistics_gatherer.py
+++ src/statistics_gatherer.py
@@ -4,7 +4,6 @@ as such, so save coding / plannign time, it will be super specific"""
 import random
 import cPickle as pickle
 import numpy as np
-import matplotlib.pyplot as plt
 from graph_manager import SnapManager
 from data_parser import ClassificationLoader
 
@@ -42,21 +41,6 @@ class StatisticsGatherer(object):
         results["nmax"] = max(result_list)
         results["nmin"] = min(result_list)
         return results
-
-    @staticmethod
-    def build_histogram(result_list, save_path, x_label="Value", y_label="Frequency", title=""):
-        bin_num = len(set(result_list))
-        plt.hist(result_list, bin_num, normed=1, facecolor='g', alpha=0.75)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.title(title)
-        plt.axis([min(result_list), max(result_list), 0, 1])
-
-        # for degugging how it looks
-        # plt.show()
-
-        # save
-        plt.savefig(save_path)
 
     @staticmethod
     def get_node_sample(graph_manager, sample_size):
@@ -124,6 +108,8 @@ class StatisticsGatherer(object):
             it also pickles everything so we can load it later"""
         loader = ClassificationLoader()
         for file_path in loader.find_files(load_folder, 0):
+            if "new25" in file_path:
+                continue
             for line in loader.lines_reader(file_path, 0):
                 loader.parse_line(line)
         if save:
@@ -140,27 +126,10 @@ def affiliation_stats_script(load_path, output_file_path):
     graph.print_info(output_file_path,
                      "Affiliation Graph POA")
 
-def run_script(load_path):
+def run_script():
     gatherer = StatisticsGatherer
 
-    # load our connected worker graph
-    graph = SnapManager().load_graph(load_path)
-
-    # build (or load) our ground truth from disk
-    ground_truth = gatherer.build_ground_truth("X:/state-csv/")
-
-    # find X plants that are of type 1
-
-
-    # find X plants that are of type 2
-
-    # for each worker in that company, calculate statistics.
-
-
-    sample = gatherer.get_node_sample(graph, 100)
-
-    gatherer.calculate_node_specific_stats(sample, graph)
-
+    ground_truth = gatherer.build_ground_truth("X:/state-csv/", "../output_stats/ground_truth_new_4d.p")
 
 
 
