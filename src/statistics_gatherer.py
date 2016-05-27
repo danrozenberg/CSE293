e0c6605455850f592f6980d4738c3dcca5f72c4c
Dan Rozenberg <danrozenberg@gmail.com>
2016-05-26 22:09:11 -0700

    lotsa good stuff

--- src/statistics_gatherer.py
+++ src/statistics_gatherer.py
@@ -2,14 +2,36 @@
 as such, so save coding / plannign time, it will be super specific"""
 
 import random
+import sys
 import cPickle as pickle
 import numpy as np
 from graph_manager import SnapManager
 from data_parser import ClassificationLoader
-
+from datetime import datetime
 
 #TODO : we don't really need a class here...
 class StatisticsGatherer(object):
+
+    @staticmethod
+    def print_datetime(stream=sys.stdout):
+        now = str(datetime.now())
+        stream.write("\ndate;" + now + "\n")
+
+    @staticmethod
+    def build_ground_truth(load_folder, output_file_path, save=True):
+        """ Builds ground truth from csv files,
+            it also pickles everything so we can load it later"""
+        loader = ClassificationLoader()
+        for file_path in loader.find_files(load_folder, 0):
+            for line in loader.lines_reader(file_path, 0):
+                loader.parse_line(line)
+        if save:
+            pickle.dump(loader.truth_data, open(output_file_path, 'wb'))
+        return loader.truth_data
+
+    @staticmethod
+    def load_ground_truth(target_file):
+        return pickle.load(open(target_file, 'rb'))
 
     @staticmethod
     def get_valid_plants(ground_truth, affiliation_graph):
@@ -29,33 +51,24 @@ class StatisticsGatherer(object):
 
     @staticmethod
     def get_statistics(result_list, name=""):
-        results = dict
-        results["name"] = name
-        results["naverage"] = np.average(result_list)
-        results["nstddev"] = np.std(result_list)
-        results["n10th percentile"] = np.percentile(result_list, 10)
-        results["n25th percentile"] = np.percentile(result_list, 25)
-        results["nmedian"] = np.median(result_list)
-        results["n75th percentile"] = np.percentile(result_list, 75)
-        results["n90th percentile"] = np.percentile(result_list, 90)
-        results["nmax"] = max(result_list)
-        results["nmin"] = min(result_list)
+        results = []
+        results.append(("name",name))
+        results.append(("average",np.average(result_list)))
+        results.append(("stddev",np.std(result_list)))
+        results.append(("10th percentile",np.percentile(result_list, 10)))
+        results.append(("25th percentile",np.percentile(result_list, 25)))
+        results.append(("median",np.median(result_list)))
+        results.append(("75th percentile",np.percentile(result_list, 75)))
+        results.append(("90th percentile",np.percentile(result_list, 90)))
+        results.append(("max",max(result_list)))
+        results.append(("min",min(result_list)))
         return results
 
     @staticmethod
-    def build_histogram(result_list, save_path, x_label="Value", y_label="Frequency", title=""):
-        bin_num = len(set(result_list))
-        plt.hist(result_list, bin_num, normed=1, facecolor='g', alpha=0.75)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.title(title)
-        plt.axis([min(result_list), max(result_list), 0, 1])
-
-        # for degugging how it looks
-        # plt.show()
-
-        # save
-        plt.savefig(save_path)
+    def print_statistics(statistics, stream):
+        for stat in statistics:
+            stream.write(stat[0] + ";" + str(stat[1]) +"\n")
+        stream.write("\n")
 
     @staticmethod
     def get_node_sample(graph_manager, sample_size):
@@ -76,98 +89,70 @@ class StatisticsGatherer(object):
         plant_id -> type"""
 
     @staticmethod
-    def calculate_node_specific_stats(sample, graph):
-        # calculate node-specific metrics
-        # it receives a sample of nodes, and will calculate
-        # the statistics of the calculated metrics.
-
-        # shorthand
+    def print_sample_specific_stats(graph, sample=None, stream=sys.stdout):
         gatherer = StatisticsGatherer
 
-        # shortests paths
-        results = map(graph.get_shortest_path_size, sample)
-        gatherer.print_statistics(results, "get_shortest_path_size")
-        # TODO: do histogram data
+        # use everything is we were not given a sample
+        if sample is None:
+            sample = graph.get_nodes()
 
-        # node centrality
-        results = map(graph.get_degree_centrality, sample)
-        gatherer.print_statistics(results, "get_degree_centrality")
+        results = gatherer.get_statistics(
+            [graph.get_shortest_path_size(n) for n in sample],
+            "shortest_path")
+        gatherer.print_statistics(results, stream)
 
-        results = map(graph.get_eccentricity, sample)
-        gatherer.print_statistics(results, "get_eccentricity")
+        results = gatherer.get_statistics(
+            [graph.get_node_degree(n) for n in sample],
+            "node_degree")
+        gatherer.print_statistics(results, stream)
 
-    @staticmethod
-    def calculate_graph_specific_metrics(graph):
+        results = gatherer.get_statistics(
+            [graph.get_clustering_coefficient(n) for n in sample],
+            "clustering_coefficient")
+        gatherer.print_statistics(results, stream)
 
-        # shorthand
-        gatherer = StatisticsGatherer
+        # TODO, use closeness maybe?
+        results = gatherer.get_statistics(
+            [graph.get_degree_centrality(n) for n in sample],
+            "degree_centrality")
+        gatherer.print_statistics(results, stream)
 
-        # node centrality
-        gatherer.print_metric(graph.get_betweeness_centrality(),
-                              "betweeness_centrality")
-
-        gatherer.print_metric(graph.get_eigenvector_centrallity(),
-                              "eigenvector_centrallity")
-
-        # connected components
-        gatherer.print_metric(graph.get_connected_components(),
-                              "connected_components")
-
-        # TRIADS AND CLUSTERING
-        gatherer.print_metric(graph.get_clustering_coefficient(),
-                              "get_clustering_coefficient")
+        # add more stuff here.
 
     @staticmethod
-    def build_ground_truth(load_folder, output_file_path, save=True):
-        """ Builds ground truth from csv files,
-            it also pickles everything so we can load it later"""
-        loader = ClassificationLoader()
-        for file_path in loader.find_files(load_folder, 0):
-            for line in loader.lines_reader(file_path, 0):
-                loader.parse_line(line)
-        if save:
-            pickle.dump(loader.truth_data, open(output_file_path, 'wb'))
-        return loader.truth_data
+    def print_graph_specific_metrics(graph, stream=sys.stdout):
+        stream.write("\n" + "nodes;" + str(graph.get_node_count()))
+        stream.write("\n" + "edges;" + str(graph.get_edge_count()))
+        stream.write("\n" + "diameter;" + str(graph.get_diameter()))
+
+
+        component_sizes = list(graph.get_connected_components())
+        stream.write("\n" + "connected components;" + str(len(component_sizes)))
+        stream.write("\n" + "component sizes;" + str(component_sizes)[1:-1])
+
 
     @staticmethod
-    def load_ground_truth(target_file):
-        return pickle.load(open(target_file, 'rb'))
+    def print_node_degree_dist(graph, stream=sys.stdout):
+        dist = graph.get_degree_dist()
+        for key in dist.keys():
+            stream.write(str(key) + ";" + str(dist[key]) + "\n")
 
-def affiliation_stats_script(load_path, output_file_path):
-    ''' saves several properties of the affiliation graph'''
-    graph = SnapManager().load_graph(load_path)
-    graph.print_info(output_file_path,
-                     "Affiliation Graph POA")
 
-def run_script(load_path):
+
+def run_script(graph, stream=sys.stdout):
     gatherer = StatisticsGatherer
-
-    # load our connected worker graph
-    graph = SnapManager().load_graph(load_path)
-
-    # build (or load) our ground truth from disk
-    ground_truth = gatherer.build_ground_truth("X:/state-csv/")
-
-    # find X plants that are of type 1
-
-
-    # find X plants that are of type 2
-
-    # for each worker in that company, calculate statistics.
-
-
-    sample = gatherer.get_node_sample(graph, 100)
-
-    gatherer.calculate_node_specific_stats(sample, graph)
-
-
-
-
-
-
-    # DO graph-as-a-whole stats
+    gatherer.print_datetime(stream)
+    # gatherer.print_graph_specific_metrics(graph, stream)
+    # gatherer.print_sample_specific_stats(graph, stream=stream)
+    gatherer.print_node_degree_dist(graph, stream)
 
 
 if __name__ == '__main__':
-    run_script()
+    graph = SnapManager()
+    graph.generate_random_graph_power_law(1000,2.0)
+    run_script(graph)
+
+    # with open("../output_stats/graph_summary.csv", 'wb') as f:
+    #     run_script(graph; f)
+
 
